d83c08719a6d8446ea92970c416a2b1342736bf3
vanya <vanya.moo@gmail.com>
2025-01-30 16:55:37 +0000

    Remove references of “items” in favour of “images” which is a domain term used by domain experts in the specs

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -17,7 +17,7 @@ public final class RemoteFeedLoader: FeedLoader {
     }
     public typealias Result = LoadFeedResult
 //    public enum Result: Equatable {
-//        case success([FeedItem])
+//        case success([FeedImage])
 //        case failure(Error)
 //    }
     
@@ -49,8 +49,8 @@ public final class RemoteFeedLoader: FeedLoader {
 }
 
 private extension Array where Element == RemoteFeedItem {
-    func toModels() -> [FeedItem] {
-        return map { FeedItem(id: $0.id, description: $0.description, location: $0.location, imageURL: $0.image)}
+    func toModels() -> [FeedImage] {
+        return map { FeedImage(id: $0.id, description: $0.description, location: $0.location, url: $0.image)}
     }
 }
 
