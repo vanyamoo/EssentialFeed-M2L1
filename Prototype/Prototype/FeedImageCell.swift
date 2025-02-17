b37f442acaf8491ed0ad2c9f8925a76bda39a10e
vanya <vanya.moo@gmail.com>
2025-02-17 16:57:18 +0000

    Add `FeedImageCell` with storyboard connections

---
+++ Prototype/Prototype/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  Prototype
+//
+//  Created by Vanya Mutafchieva on 17/02/2025.
+//
+
+import UIKit
+
+final class FeedImageCell: UITableViewCell {
+	@IBOutlet private(set) var locationContainer: UIView!
+	@IBOutlet private(set) var locationLabel: UILabel!
+	@IBOutlet private(set) var feedImageView: UIImageView!
+	@IBOutlet private(set) var descriptionLabel: UILabel!
+}
--- Prototype/Prototype/FeedViewController.swift
+++ Prototype/Prototype/FeedViewController.swift
@@ -7,15 +7,37 @@
 
 import UIKit
 
+struct FeedImageViewModel {
+    let description: String?
+    let location: String?
+    let imageName: String
+}
+
 
 final class FeedViewController: UITableViewController {
+    
+    private let feed = FeedImageViewModel.prototypeFeed
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return feed.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "FeedImageCell", for: indexPath) as! FeedImageCell
+        let model = feed[indexPath.row]
+        cell.configure(with: model)
+        return cell
+    }
+}
 
-	override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-		return 10
-	}
+extension FeedImageCell {
+    func configure(with model: FeedImageViewModel) {
+        locationLabel.text = model.location
+        locationContainer.isHidden = model.location == nil
 
-	override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-		return tableView.dequeueReusableCell(withIdentifier: "FeedImageCell")!
-	}
+        descriptionLabel.text = model.description
+        descriptionLabel.isHidden = model.description == nil
 
+        feedImageView.image = UIImage(named: model.imageName)
+    }
 }
