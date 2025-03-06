0bc899ddf21b698b27070b7bfee4c22366c5a302
vanya <vanya.moo@gmail.com>
2025-03-06 15:29:59 +0000

    Move ViewModels to separate files

---
+++ EssentialFeed/EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeed
+//
+//  Created by Vanya Mutafchieva on 06/03/2025.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
