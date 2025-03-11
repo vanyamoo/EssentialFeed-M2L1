55067c3174c6ea4f1fb2451a767c4ba5c5899ef0
vanya <vanya.moo@gmail.com>
2025-03-11 15:21:53 +0000

    Localize `Feed.strings` file

---
+++ EssentialFeed/EssentialFeediOSTests/Helpers/FeedViewControllerTests+Localization.swift
@@ -0,0 +1,22 @@
+//
+//  FeedUIIntegrationTests+Localization.swift
+//  EssentialFeed
+//
+//  Created by Vanya Mutafchieva on 11/03/2025.
+//
+
+import Foundation
+import XCTest
+import EssentialFeediOS
+
+extension FeedUIIntegrationTests {
+    func localized(_ key: String, file: StaticString = #file, line: UInt = #line) -> String {
+        let table = "Feed"
+        let bundle = Bundle(for: FeedViewController.self)
+        let value = bundle.localizedString(forKey: key, value: nil, table: table)
+        if value == key {
+            XCTFail("Missing localized string for key: \(key) in table: \(table)", file: file, line: line)
+        }
+        return value
+    }
+}
