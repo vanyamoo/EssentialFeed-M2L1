b4df830d070ed6040a54dcf64c8022c0bbba1ead
vanya <vanya.moo@gmail.com>
2025-03-28 11:27:45 +0000

    Move `FeedLoadingViewModel` to separate file

---
+++ EssentialFeed/EssentialFeed/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,10 @@
+//
+//  FeedLoadingViewModel.swift
+//  EssentialFeed
+//
+//  Created by Vanya Mutafchieva on 28/03/2025.
+//
+
+public struct FeedLoadingViewModel {
+    public let isLoading: Bool
+}
