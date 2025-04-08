8fe033278c67bfcf18b1a55c8bf9a1aa85dd2535
vanya <vanya.moo@gmail.com>
2025-04-08 12:26:46 +0100

    Revert "Check number of cells before fetching cell at index to avoid out of bounds exception"
    This reverts commit 8f7456764d3010f0bf90d21f41826d01bee37d75.

---
+++ EssentialApp/EssentialAppUIAcceptanceTests/EssentialAppUIAcceptanceTestsLaunchTests.swift
@@ -0,0 +1,13 @@
+//
+//  EssentialAppUIAcceptanceTestsLaunchTests.swift
+//  EssentialAppUIAcceptanceTests
+//
+//  Created by Vanya Mutafchieva on 04/04/2025.
+//
+
+import XCTest
+
+final class EssentialAppUIAcceptanceTestsLaunchTests: XCTestCase {
+
+}
+
